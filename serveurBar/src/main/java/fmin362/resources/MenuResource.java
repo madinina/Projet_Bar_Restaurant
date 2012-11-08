f0179d96ecd95dbcec1a57a7eba647979bb50428
atazouti <atazouti@si21.etu.info-ufr.univ-montp2.fr>
2012-11-08 10:54:30 +0100

    Creation categories

--- serveurBar/src/main/java/fmin362/resources/MenuResource.java
+++ serveurBar/src/main/java/fmin362/resources/MenuResource.java
@@ -15,13 +15,23 @@ public class MenuResource {
 	@GET
 	@Produces( MediaType.APPLICATION_JSON )
 	public List<Produit> menu(){
-		Produit p1=new Produit();
-		p1.setNom("Bière");
-		Produit p2=new Produit();
-		p2.setNom("Coca");
+		
 		List<Produit> liste=new ArrayList<Produit>();
+		Produit p1=new Produit();
+		Produit p2=new Produit();
+		Produit p3=new Produit();
+		
+		p1.setNom("Bière");
+		p2.setNom("Coca");
+		p3.setNom("Lasagne");
+		
+		p1.setCategorie(new Categorie("Boisson alcolisée"));
+		p2.setCategorie(new Categorie("Boisson  non alcolisée"));
+		p3.setCategorie(new Categorie("Plat chaud"));
+		
 		liste.add(p1);
 		liste.add(p2);
+		liste.add(p3);
 		return liste;
 		
 		
